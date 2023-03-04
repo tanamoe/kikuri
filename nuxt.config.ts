4c325b878c22d138973fd6bb93bbfbd6332dc7d6
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-03-04 15:26:40 +0700

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // https://nuxt.com/docs/getting-started/installation#prerequisites
+  typescript: {
+    shim: false,
+  },
+});
