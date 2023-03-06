8caff744949fd75cf1d7aaa056fb411068ad4129
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-03-06 19:02:05 +0700

    build: added `cva`, `tailwind` with plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss"],
   // https://nuxt.com/docs/getting-started/installation#prerequisites
   typescript: {
     shim: false,
