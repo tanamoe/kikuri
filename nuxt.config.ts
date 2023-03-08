e99a41d14559b942c59b943b2f8ca1217bc31cb5
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-03-09 00:11:32 +0700

    build: added `nuxt-image`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@vueuse/nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image-edge",
+    "nuxt-icon",
+    "@vueuse/nuxt",
+  ],
   // https://nuxt.com/docs/getting-started/installation#prerequisites
   typescript: {
     shim: false,
