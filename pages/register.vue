817fe9e0be82854aeb258ef6f9f108df0ceb8670
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-04-02 20:12:06 +0700

    fix: `register` header to "Đăng ký"

--- pages/register.vue
+++ pages/register.vue
@@ -9,7 +9,7 @@ definePageMeta({
     <div
       class="w-full min-w-fit max-w-sm rounded-lg bg-zinc-200 p-6 dark:bg-zinc-700"
     >
-      <h2 class="font-lexend mb-6 text-4xl font-bold">Đăng nhập</h2>
+      <h2 class="font-lexend mb-6 text-4xl font-bold">Đăng ký</h2>
       <form class="space-y-3">
         <div class="space-y-1">
           <label class="block" for="username">Tên tài khoản</label>
