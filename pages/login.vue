8a9e756c1aaa054f74be345eb2dc0f8c7a5195a0
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-07-04 23:20:37 +0700

    feat: implement auth pages

--- pages/login.vue
+++ pages/login.vue
@@ -1,53 +1,130 @@
+<script setup lang="ts">
+import { ClientResponseError, Record } from "pocketbase";
+
+const { $pb } = useNuxtApp();
+
+const loading = ref(false);
+const alert = ref<{
+  intent: "success" | "error";
+  message: string;
+  show: boolean;
+}>({
+  intent: "error",
+  message: "",
+  show: false,
+});
+const user = ref("");
+const password = ref("");
+
+const loginWithPassword = async () => {
+  loading.value = true;
+
+  if (user.value === "") {
+    alert.value.intent = "error";
+    alert.value.message = "Email không được để trống";
+    alert.value.show = true;
+  } else if (password.value === "") {
+    alert.value.intent = "error";
+    alert.value.message = "Mật khẩu không được để trống";
+    alert.value.show = true;
+  } else {
+    try {
+      await $pb
+        .collection("users")
+        .authWithPassword(user.value, password.value);
+
+      alert.value.intent = "success";
+      alert.value.message = "Đăng nhập thành công, đang chuyển hướng...";
+      alert.value.show = true;
+
+      navigateTo("/");
+    } catch (err) {
+      if (err instanceof ClientResponseError) {
+        alert.value.intent = "error";
+        alert.value.message = `Lỗi: ${err.message}`;
+        alert.value.show = true;
+      }
+    }
+  }
+
+  loading.value = false;
+};
+
+definePageMeta({
+  middleware: [
+    () => {
+      const { $pb } = useNuxtApp();
+
+      if ($pb.authStore.model instanceof Record) {
+        return navigateTo("/");
+      }
+    },
+  ],
+});
+</script>
+
 <template>
   <div class="absolute inset-0">
-    <div class="flex h-full items-center justify-center overflow-y-scroll">
+    <div
+      class="flex h-full flex-col items-center justify-center gap-6 overflow-y-scroll"
+    >
       <div
-        class="w-full min-w-fit max-w-sm rounded-lg bg-zinc-200 p-6 dark:bg-zinc-700"
+        class="w-full min-w-fit max-w-sm rounded-lg bg-zinc-200 p-6 dark:bg-zinc-700/50"
       >
-        <h2 class="font-lexend mb-6 text-4xl font-bold">Đăng nhập</h2>
-        <form class="space-y-3">
-          <div class="space-y-1">
-            <label class="block" for="email">Email</label>
-            <input
-              id="email"
-              class="focus:ring-primary w-full rounded-xl border-none bg-zinc-300 transition-all invalid:ring-2 invalid:ring-red-400 focus:bg-zinc-100 focus:ring-2 dark:bg-zinc-600 focus:dark:bg-zinc-700"
-              type="email"
-              placeholder="konnichiwa@glhf.vn"
-            />
+        <AppHeading>Đăng nhập</AppHeading>
+        <form @submit.prevent="loginWithPassword">
+          <div class="space-y-3">
+            <div class="space-y-1">
+              <label class="block" for="user">Email/tên tài khoản</label>
+              <AppInput
+                id="user"
+                v-model="user"
+                type="text"
+                placeholder="kikuri"
+              />
+            </div>
+            <div class="space-y-1">
+              <label class="block" for="password">Mật khẩu</label>
+              <AppInput
+                id="password"
+                v-model="password"
+                type="password"
+                placeholder="dotdotdot"
+              />
+            </div>
           </div>
-          <div class="space-y-1">
-            <label class="block" for="password">Mật khẩu</label>
-            <input
-              id="password"
-              class="focus:ring-primary w-full rounded-xl border-none bg-zinc-300 transition-all focus:bg-zinc-100 focus:ring-2 dark:bg-zinc-600 focus:dark:bg-zinc-700"
-              type="password"
-              placeholder="dotdotdot"
-            />
-          </div>
-          <div class="flex items-center gap-3">
-            <input
-              id="remember"
-              type="checkbox"
-              class="text-primary focus:ring-primary rounded bg-transparent transition-all focus:ring-2"
-            />
-            <label for="remember">Lưu đăng nhập</label>
-          </div>
-          <AppButton class="w-full" intent="primary">Đăng nhập</AppButton>
-          <div class="text-center text-zinc-600 dark:text-zinc-400">
+          <AppButton
+            class="mt-6 w-full"
+            :intent="loading ? 'secondary' : 'primary'"
+            @submit="loginWithPassword"
+          >
+            <span v-if="loading">Đang tải...</span>
+            <span v-else>Đăng nhập</span>
+          </AppButton>
+          <div class="mt-3 text-center text-zinc-600 dark:text-zinc-400">
             <NuxtLink
               class="decoration-primary decoration-2 hover:underline"
               to="/forget"
-              >Quên mật khẩu?</NuxtLink
             >
+              Quên mật khẩu?
+            </NuxtLink>
             &middot;
             <NuxtLink
               class="decoration-primary decoration-2 hover:underline"
               to="/register"
-              >Đăng ký</NuxtLink
             >
+              Đăng ký
+            </NuxtLink>
           </div>
         </form>
       </div>
+
+      <AppAlert
+        :class="[alert.show ? '' : 'hidden', 'max-w-sm']"
+        :intent="alert.intent"
+      >
+        {{ alert.message }}
+      </AppAlert>
     </div>
   </div>
 </template>
--- pages/register.vue
+++ pages/register.vue
@@ -1,48 +1,157 @@
+<script setup lang="ts">
+import { ClientResponseError, Record } from "pocketbase";
+
+const { $pb } = useNuxtApp();
+
+const loading = ref(false);
+const alert = ref<{
+  intent: "success" | "error";
+  message: string;
+  show: boolean;
+}>({
+  intent: "error",
+  message: "",
+  show: false,
+});
+const username = ref("");
+const email = ref("");
+const password = ref("");
+const passwordConfirm = ref("");
+
+const register = async () => {
+  loading.value = true;
+
+  if (username.value === "") {
+    alert.value.intent = "error";
+    alert.value.message = "Tên tài khoản không được để trống";
+    alert.value.show = true;
+  } else if (email.value === "") {
+    alert.value.intent = "error";
+    alert.value.message = "Email không được để trống";
+    alert.value.show = true;
+  } else if (password.value === "" || passwordConfirm.value === "") {
+    alert.value.intent = "error";
+    alert.value.message = "Mật khẩu không được để trống";
+    alert.value.show = true;
+  } else if (password.value !== passwordConfirm.value) {
+    alert.value.intent = "error";
+    alert.value.message = "Mật khẩu không trùng";
+    alert.value.show = true;
+  } else {
+    try {
+      await $pb.collection("users").create({
+        username: username.value,
+        email: email.value,
+        password: password.value,
+        passwordConfirm: passwordConfirm.value,
+      });
+
+      await $pb.collection("users").requestVerification(email.value);
+
+      await $pb
+        .collection("users")
+        .authWithPassword(username.value, password.value);
+
+      alert.value.intent = "success";
+      alert.value.message = "Đăng ký thành công, đang chuyển hướng...";
+      alert.value.show = true;
+
+      navigateTo("/");
+    } catch (err) {
+      if (err instanceof ClientResponseError) {
+        alert.value.intent = "error";
+        alert.value.message = `Lỗi: ${err.message}`;
+        alert.value.show = true;
+      }
+    }
+  }
+
+  loading.value = false;
+};
+
+definePageMeta({
+  middleware: [
+    () => {
+      const { $pb } = useNuxtApp();
+
+      if ($pb.authStore.model instanceof Record) {
+        return navigateTo("/");
+      }
+    },
+  ],
+});
+</script>
+
 <template>
   <div class="absolute inset-0">
-    <div class="flex h-full items-center justify-center overflow-y-scroll">
+    <div
+      class="flex h-full flex-col items-center justify-center gap-6 overflow-y-scroll"
+    >
       <div
-        class="w-full min-w-fit max-w-sm rounded-lg bg-zinc-200 p-6 dark:bg-zinc-700"
+        class="w-full min-w-fit max-w-sm rounded-lg bg-zinc-200 p-6 dark:bg-zinc-700/50"
       >
-        <h2 class="font-lexend mb-6 text-4xl font-bold">Đăng ký</h2>
-        <form class="space-y-3">
-          <div class="space-y-1">
-            <label class="block" for="username">Tên tài khoản</label>
-            <input
-              id="username"
-              class="focus:ring-primary w-full rounded-xl border-none bg-zinc-300 transition-all invalid:ring-2 invalid:ring-red-400 focus:bg-zinc-100 focus:ring-2 dark:bg-zinc-600 focus:dark:bg-zinc-700"
-              type="text"
-              placeholder="kikuri"
-            />
+        <AppHeading>Đăng ký</AppHeading>
+        <form @submit.prevent="register">
+          <div class="space-y-3">
+            <div class="space-y-1">
+              <label class="block" for="username">Tên tài khoản</label>
+              <AppInput
+                id="username"
+                v-model="username"
+                type="text"
+                placeholder="kikuri"
+              />
+            </div>
+            <div class="space-y-1">
+              <label class="block" for="email">Email</label>
+              <AppInput
+                id="email"
+                v-model="email"
+                type="email"
+                placeholder="konnichiwa@tana.moe"
+              />
+            </div>
+            <div class="space-y-1">
+              <label class="block" for="password">Mật khẩu</label>
+              <AppInput
+                id="password"
+                v-model="password"
+                type="password"
+                placeholder="dotdotdot"
+              />
+            </div>
+            <div class="space-y-1">
+              <label class="block" for="passwordConfirm"
+                >Nhập lại mật khẩu</label
+              >
+              <AppInput
+                id="passwordConfirm"
+                v-model="passwordConfirm"
+                type="password"
+                placeholder="dotdotdot"
+              />
+            </div>
           </div>
-          <div class="space-y-1">
-            <label class="block" for="email">Email</label>
-            <input
-              id="email"
-              class="focus:ring-primary w-full rounded-xl border-none bg-zinc-300 transition-all invalid:ring-2 invalid:ring-red-400 focus:bg-zinc-100 focus:ring-2 dark:bg-zinc-600 focus:dark:bg-zinc-700"
-              type="email"
-              placeholder="konnichiwa@glhf.vn"
-            />
-          </div>
-          <div class="space-y-1">
-            <label class="block" for="password">Mật khẩu</label>
-            <input
-              id="password"
-              class="focus:ring-primary w-full rounded-xl border-none bg-zinc-300 transition-all focus:bg-zinc-100 focus:ring-2 dark:bg-zinc-600 focus:dark:bg-zinc-700"
-              type="password"
-              placeholder="dotdotdot"
-            />
-          </div>
-          <AppButton class="w-full" intent="primary">Đăng ký</AppButton>
-          <div class="text-center text-zinc-600 dark:text-zinc-400">
+          <AppButton class="mt-6 w-full" intent="primary" @click="register"
+            >Đăng ký</AppButton
+          >
+          <div class="mt-3 text-center text-zinc-600 dark:text-zinc-400">
             <NuxtLink
               class="decoration-primary decoration-2 hover:underline"
               to="/login"
-              >Đăng nhập</NuxtLink
             >
+              Đăng nhập
+            </NuxtLink>
           </div>
         </form>
       </div>
+
+      <AppAlert
+        :class="[alert.show ? '' : 'hidden', 'max-w-sm']"
+        :intent="alert.intent"
+      >
+        {{ alert.message }}
+      </AppAlert>
     </div>
   </div>
 </template>
