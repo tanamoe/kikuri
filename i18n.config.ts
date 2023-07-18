c3c322a7c9a74e9ebb27f6aa0f34f16bcc8ef45a
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-07-18 10:54:08 +0700

    build: update i18n

---
+++ i18n.config.ts
@@ -0,0 +1,38 @@
+export default defineI18nConfig(() => {
+  return {
+    legacy: false,
+    numberFormats: {
+      vi: {
+        currency: {
+          style: "currency",
+          currency: "VND",
+          notation: "standard",
+        },
+      },
+    },
+    datetimeFormats: {
+      vi: {
+        weekdayShort: {
+          weekday: "long",
+        },
+        twoDigitDay: {
+          day: "2-digit",
+        },
+        twoDigitMonth: {
+          month: "2-digit",
+        },
+      },
+      en: {
+        weekdayShort: {
+          weekday: "short",
+        },
+        twoDigitDay: {
+          day: "2-digit",
+        },
+        twoDigitMonth: {
+          month: "2-digit",
+        },
+      },
+    },
+  };
+});
