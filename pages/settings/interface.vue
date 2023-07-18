e445396747a2144680d06c72d29922877787fef1
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-07-18 14:32:27 +0700

    feat: implement interface settings

--- pages/settings/interface.vue
+++ pages/settings/interface.vue
@@ -2,9 +2,25 @@
 import { storeToRefs } from "pinia";
 import { useSettingsStore } from "@/stores/settings";
 
+const { t } = useI18n({ useScope: "global" });
 const store = useSettingsStore();
 
 const { settings } = storeToRefs(store);
+
+const digitalOptions = [
+  {
+    name: t("calendar.showDigital"),
+    value: "show",
+  },
+  {
+    name: t("calendar.hideDigital"),
+    value: "hide",
+  },
+  {
+    name: t("calendar.onlyDigital"),
+    value: "only",
+  },
+];
 
 definePageMeta({
   layout: "setting",
@@ -13,12 +29,19 @@ definePageMeta({
 
 <template>
   <div>
-    <form ref="passwordForm" class="space-y-6" @submit.prevent="">
+    <form ref="passwordForm" class="space-y-6">
       <UFormGroup
         name="showBookDetails"
         :label="$t('interface.showBookDetails')"
       >
         <UToggle v-model="settings.showBookDetails" />
+      </UFormGroup>
+      <UFormGroup name="digital" :label="$t('calendar.digitalOptions')">
+        <USelect
+          v-model="settings.showDigital"
+          :options="digitalOptions"
+          option-attribute="name"
+        />
       </UFormGroup>
     </form>
   </div>
