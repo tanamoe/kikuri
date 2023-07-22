6c956ae8bd3a657718d96154afc860be3b1ad0e0
Nguyen Do <khoanguyen.do@outlook.com>
2023-07-22 14:15:28 +0700

    feat: implement i18n for error page & fix auth pages layout for newly added footer (#86)
    * fix: change auth pages height
    
    * feat: i18n for error page

--- error.vue
+++ error.vue
@@ -1,34 +1,63 @@
-<script setup>
+<script setup lang="ts">
 const props = defineProps({
   // eslint-disable-next-line vue/require-default-prop
   error: Object,
 });
 
-const is404 = computed(() => props.error.statusCode === 404);
+const is404 = computed(() => props.error!.statusCode === 404);
 const isDev = process.dev;
+
+const i18nHead = useLocaleHead({
+  addSeoAttributes: true,
+});
+
+useHead({
+  htmlAttrs: {
+    lang: i18nHead.value.htmlAttrs!.lang,
+  },
+  bodyAttrs: {
+    class: "dark:bg-gray-900 bg-gray-50 text-gray-700 dark:text-gray-200",
+  },
+  link: [...(i18nHead.value.link || [])],
+  meta: [...(i18nHead.value.meta || [])],
+});
 
 const handleError = () => clearError({ redirect: "/" });
 </script>
 
 <template>
   <NuxtLayout>
-    <div class="absolute inset-0">
-      <div class="flex h-screen items-center justify-center">
-        <div class="space-y-6 text-center">
-          <h1 class="font-lexend text-8xl font-bold">
-            {{ error.statusCode }}
-          </h1>
-          <p>
-            {{ is404 ? "Không tìm thấy đường dẫn" : "Đã có lỗi xảy ra" }}, bạn
-            có muốn
-            <button class="font-bold text-tanablue-500" @click="handleError">
-              về trang chủ</button
-            >?
-          </p>
-          <p v-if="isDev">
-            {{ error }}
-          </p>
+    <div class="flex h-[80vh] items-center justify-center">
+      <div class="space-y-6 text-center">
+        <h1 class="font-lexend text-8xl font-bold">
+          {{ error!.statusCode }}
+        </h1>
+        <p>
+          <span v-if="is404">{{ $t("error.notFoundMessage") }}</span>
+          <span v-else>{{ $t("error.generalMessage") }}</span>
+        </p>
+        <div class="flex justify-center gap-3">
+          <UButton color="black" @click="handleError">
+            {{ $t("error.returnHome") }}
+          </UButton>
+          <UButton
+            to="https://youtu.be/2b1IexhKPz4"
+            target="_blank"
+            color="kyukurarin"
+            :ui="{
+              color: {
+                kyukurarin: {
+                  solid: 'bg-[#fec9d7] text-gray-800',
+                },
+              },
+            }"
+          >
+            Kyu-kurarin
+          </UButton>
         </div>
+        <p v-if="isDev">
+          {{ error }}
+        </p>
       </div>
     </div>
   </NuxtLayout>
