2f031673cd43df1d2de8f7220425f9df5e9c9f21
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-08-05 22:06:52 +0700

    feat: implement language switcher notification

--- components/AppLanguageSwitcher.vue
+++ components/AppLanguageSwitcher.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import type { LocaleObject } from "@nuxtjs/i18n/dist/runtime/composables";
 
-const { locale, locales, setLocale } = useI18n();
+const { t, locale, locales, setLocale } = useI18n({ useScope: "global" });
+const toast = useToast();
+
+type Locale = {
+  id: string;
+  label: string;
+};
 
 const availableLocales = computed(() => {
   return (locales.value as LocaleObject[]).map((l) => ({
@@ -10,15 +16,24 @@ const availableLocales = computed(() => {
   }));
 });
 
-const currentLocale = ref(
+const currentLocale = ref<Locale | undefined>(
   availableLocales.value.find((i) => i.id === locale.value)
 );
+
+const changeLocale = async (v: Locale) => {
+  await setLocale(v!.id);
+  toast.add({
+    icon: "i-fluent-local-language-20-regular",
+    title: t("general.localeChanged"),
+    description: t("general.localeChangedDescription"),
+  });
+};
 </script>
 
 <template>
   <USelectMenu
     v-model="currentLocale"
     :options="availableLocales"
-    @update:model-value="(v: typeof currentLocale) => setLocale(v!.id)"
+    @update:model-value="changeLocale"
   />
 </template>
