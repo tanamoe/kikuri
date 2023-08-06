b883765f7350b0692d33f7c26034b2d2e0033fd0
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-08-06 15:26:36 +0700

    feat: implement oauth login composables

--- composables/index.ts
+++ composables/index.ts
@@ -1,5 +1,10 @@
 export { useCalendar, useRecentReleases } from "./pocketbase/calendar";
-export { useLogin, usePasswordReset, useRegister } from "./pocketbase/auth";
+export {
+  useLogin,
+  useOAuthLogin,
+  usePasswordReset,
+  useRegister,
+} from "./pocketbase/auth";
 export { useUpdateAccount, useRequestVerification } from "./pocketbase/account";
 export { useRecentBooks, useAsyncBookDetailed } from "./pocketbase/book";
 export {
