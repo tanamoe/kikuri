36069751fac373a950c98d6f70beea529b9d2ac3
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-08-11 20:49:11 +0700

    feat: update title page to use gallery

--- components/PageTitle/PageTitleCoverImages.vue
+++ components/PageTitle/PageTitleCoverImages.vue
@@ -1,24 +1,53 @@
 <script setup lang="ts">
 import type { BaseSystemFields } from "types/pb";
 
-defineProps<{
+const props = defineProps<{
   images: (Pick<BaseSystemFields, "id" | "collectionId"> & {
     cover: string[];
   })[];
 }>();
+
+const parsedImages = computed(() => {
+  const images: string[] = [];
+
+  props.images.map((image) =>
+    image.cover.map((cover) =>
+      images.push(getPockerBaseImagePath(image, cover)),
+    ),
+  );
+
+  return images;
+});
+
+const isOpen = ref(false);
+const currentIndex = ref(0);
+
+const handleOpen = (i: number) => {
+  isOpen.value = true;
+  currentIndex.value = i;
+};
 </script>
 
 <template>
   <div class="grid grid-cols-2 gap-6 sm:grid-cols-4 xl:grid-cols-6">
-    <template v-for="image in images" :key="image.id">
-      <AppCard v-for="cover in image.cover" :key="cover">
-        <NuxtImg
-          loading="lazy"
-          class="aspect-[2/3] h-full w-full object-cover"
-          :src="getPockerBaseImagePath(image, cover)"
-          sizes="sm:50vw md:30vw lg:20vw"
-        />
-      </AppCard>
-    </template>
+    <AppCard
+      v-for="(cover, i) in parsedImages"
+      :key="cover"
+      class="cursor-zoom-in"
+      @click="handleOpen(i)"
+    >
+      <NuxtImg
+        loading="lazy"
+        class="aspect-[2/3] h-full w-full object-cover"
+        :src="cover"
+        sizes="sm:50vw md:30vw lg:20vw"
+      />
+    </AppCard>
+
+    <AppGallery
+      v-model="isOpen"
+      v-model:current-index="currentIndex"
+      :images="parsedImages"
+    />
   </div>
 </template>
