319334e9394a3e1890b849d6f6445e82505f78ae
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-08-12 21:43:25 +0700

    feat: implement OAuth2 code login

---
+++ pages/redirect.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { Record, type AuthProviderInfo } from "pocketbase";
+import { joinURL } from "ufo";
+
+const runtimeConfig = useRuntimeConfig();
+const { query } = useRoute();
+const { t } = useI18n({ useScope: "global" });
+const authProvider = useCookie<AuthProviderInfo>("auth_provider");
+
+if (authProvider.value.state !== query.state)
+  throw createError(t("auth.oauthInvalidState"));
+
+const res = await loginWithOAuth2(
+  authProvider.value.name,
+  query.code as string,
+  authProvider.value.codeVerifier,
+  joinURL(runtimeConfig.public.siteUrl, "redirect"),
+);
+
+if (res) navigateTo("/");
+
+definePageMeta({
+  middleware: [
+    () => {
+      const { $pb } = useNuxtApp();
+
+      if ($pb.authStore.model instanceof Record) {
+        return navigateTo("/");
+      }
+    },
+  ],
+});
+</script>
+
+<template>
+  <UContainer
+    class="my-12 flex h-[80vh] flex-col items-center justify-center gap-6"
+  >
+    <Head>
+      <Title>{{ $t("account.login") }}</Title>
+    </Head>
+
+    <div class="w-full max-w-sm rounded-lg bg-gray-100 p-6 dark:bg-gray-800">
+      <AppH1 class="mb-6">{{ $t("account.login") }}</AppH1>
+      <p class="mb-6">{{ $t("auth.oauthPending") }}</p>
+      <UButton to="/login">{{ $t("auth.oauthStuck") }}</UButton>
+    </div>
+  </UContainer>
+</template>
---
+++ utils/auth.ts
@@ -0,0 +1,50 @@
+import { ClientResponseError } from "pocketbase";
+import { Collections, type UsersResponse } from "@/types/pb";
+
+export async function getAuthMethods() {
+  const { $pb } = useNuxtApp();
+
+  const authMethods = await $pb.collection("users").listAuthMethods();
+
+  return structuredClone(authMethods);
+}
+
+export async function loginWithOAuth2(
+  provider: string,
+  code: string,
+  codeVerifier: string,
+  redirectUrl: string,
+) {
+  const { $pb } = useNuxtApp();
+  const { t } = useI18n({ useScope: "global" });
+  const toast = useToast();
+
+  try {
+    const response = await $pb
+      .collection(Collections.Users)
+      .authWithOAuth2Code<UsersResponse>(
+        provider,
+        code,
+        codeVerifier,
+        redirectUrl,
+      );
+
+    toast.add({
+      color: "primary",
+      title: t("auth.loginSuccessful"),
+      description: t("auth.redirecting"),
+      icon: "i-fluent-checkmark-circle-20-filled",
+    });
+
+    return response;
+  } catch (err) {
+    if (err instanceof ClientResponseError) {
+      toast.add({
+        color: "red",
+        title: t("general.error"),
+        description: err.message,
+        icon: "i-fluent-error-circle-20-filled",
+      });
+    }
+  }
+}
