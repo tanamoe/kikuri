3714a892db96f2b2400f53a0f2414d49af5b0e37
Nguyen Do <khoanguyen.do@outlook.com>
2023-09-11 12:40:15 +0700

    feat: implement social links component (#192)

---
+++ components/AppSocialLinks.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const appConfig = useAppConfig();
+</script>
+
+<template>
+  <ul class="flex items-center gap-1">
+    <li v-for="social in appConfig.tana.socials" :key="social.name">
+      <UButton
+        :icon="social.icon"
+        :to="social.url"
+        target="_blank"
+        color="gray"
+        variant="ghost"
+        size="sm"
+        square
+      />
+    </li>
+  </ul>
+</template>
