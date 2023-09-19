3fa1216925016a0752eb376a2425f7862a16ff02
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-09-20 02:25:31 +0700

    feat(settings): implement new layout

--- layouts/setting.vue
+++ layouts/setting.vue
@@ -1,32 +1,5 @@
 <script setup lang="ts">
-const { $pb } = useNuxtApp();
-const { t } = useI18n({ useScope: "global" });
 const route = useRoute();
-
-const general = [
-  {
-    label: t("settings.profile"),
-    to: "/settings/profile",
-  },
-];
-
-const site = [
-  {
-    label: t("settings.interface"),
-    to: "/settings/interface",
-  },
-];
-
-const account = [
-  {
-    label: t("settings.account"),
-    to: "/settings/account",
-  },
-  {
-    label: t("settings.password"),
-    to: "/settings/password",
-  },
-];
 
 const sticky = computed(() => route.meta.stickyNav as boolean);
 </script>
@@ -37,18 +10,7 @@ const sticky = computed(() => route.meta.stickyNav as boolean);
     <main class="container mx-auto min-h-[80vh] overflow-x-hidden px-6 pb-6">
       <AppH1 class="mb-6">{{ $t("general.settings") }}</AppH1>
       <div class="flex flex-col gap-6 sm:flex-row">
-        <div
-          class="flex gap-3 overflow-x-scroll whitespace-nowrap sm:w-48 sm:flex-col"
-        >
-          <div v-if="$pb.authStore.isAuthRecord">
-            <UVerticalNavigation :links="general" />
-            <hr class="dark:border-gray-700" />
-          </div>
-          <UVerticalNavigation :links="site" />
-          <div v-if="$pb.authStore.isAuthRecord">
-            <UVerticalNavigation :links="account" />
-          </div>
-        </div>
+        <TheSettingNavigation class="sm:w-48" />
         <div class="flex-1">
           <slot />
         </div>
