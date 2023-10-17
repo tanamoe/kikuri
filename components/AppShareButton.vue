8b843c16a068933e2adc04dbf9c35a0d48776201
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-10-17 17:32:37 +0700

    fix(pb): changes to pb back-relations

---
+++ components/AppShareButton.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+}>();
+
+const { share, isSupported: shareSupported } = useShare();
+const { copy, copied, isSupported: clipboardSupported } = useClipboard();
+const { t } = useI18n({ useScope: "global" });
+const toast = useToast();
+
+function startShare() {
+  share({ title: props.title, url: location.href });
+}
+
+function copyLink() {
+  toast.add({
+    color: "primary",
+    title: t("general.copySuccess"),
+    icon: "i-fluent-checkmark-circle-20-filled",
+  });
+  copy(location.href);
+}
+</script>
+
+<template>
+  <UButton
+    v-if="shareSupported"
+    icon="i-fluent-share-ios-20-regular"
+    color="gray"
+    square
+    @click="startShare"
+  />
+  <UButton
+    v-else-if="clipboardSupported"
+    :icon="
+      copied ? 'i-fluent-checkmark-20-filled' : 'i-fluent-link-square-20-filled'
+    "
+    color="gray"
+    square
+    @click="copyLink"
+  />
+</template>
--- pages/title/[id].vue
+++ pages/title/[id].vue
@@ -16,95 +16,66 @@ const route = useRoute();
 const runtimeConfig = useRuntimeConfig();
 const { $pb } = useNuxtApp();
 const { t } = useI18n({ useScope: "global" });
-const { share, isSupported: shareSupported } = useShare();
-const { copy, copied, isSupported: clipboardSupported } = useClipboard();
-const toast = useToast();
 
-const { data: title } = await useAsyncData(() =>
+const { data } = await useAsyncData(() =>
   $pb.collection(Collections.Titles).getOne<
     TitlesResponse<
       {},
       {
+        "works(title)": WorksResponse<{
+          staff: StaffsResponse;
+        }>[];
+        "releases(title)": ReleasesResponse<{
+          publisher: PublishersResponse;
+        }>[];
         format: FormatsResponse;
       }
     >
   >(route.params.id as string, {
-    expand: "format",
+    expand: "works(title).staff,releases(title).publisher,format",
   }),
 );
-if (!title.value)
+
+const releases = computed(() => data.value?.expand?.["releases(title)"]);
+const works = computed(() => data.value?.expand?.["works(title)"]);
+
+if (!data.value)
   throw createError({
     statusCode: 404,
     statusMessage: t("error.notFoundMessage"),
   });
 
-const { data: works } = await useAsyncData(() =>
-  $pb.collection(Collections.Works).getFullList<
-    WorksResponse<{
-      staff: StaffsResponse;
-    }>
-  >({
-    filter: `title = '${title.value!.id}'`,
-    sort: "+priority",
-    expand: "staff",
-  }),
-);
-
-const { data: releases } = await useAsyncData(() =>
-  $pb.collection(Collections.Releases).getFullList<
-    ReleasesResponse<{
-      publisher: PublishersResponse;
-    }>
-  >({
-    filter: `title='${title.value!.id}'`,
-    expand: "publisher",
-  }),
-);
-
-const { data: reviews } = await useAsyncData(() =>
+const { data: reviews } = await useLazyAsyncData(() =>
   $pb.collection(Collections.Reviews).getFullList<
     ReviewsResponse<{
       user: UsersResponse;
     }>
   >({
-    filter: `release.title = '${title.value!.id}'`,
+    filter: `release.title = '${data.value!.id}'`,
     expand: "user",
   }),
 );
 
-const { data: images } = await useAsyncData(() =>
-  getTitleCoverImages(title.value!.id),
+const { data: images } = await useLazyAsyncData(() =>
+  getTitleCoverImages(data.value!.id),
 );
 
-function startShare() {
-  share({ title: title.value!.name, url: location.href });
-}
-
-function copyLink() {
-  toast.add({
-    color: "primary",
-    title: t("general.copySuccess"),
-    icon: "i-fluent-checkmark-circle-20-filled",
-  });
-  copy(location.href);
-}
-
 const ogImage = new URL("/title", runtimeConfig.public.ogUrl);
-ogImage.searchParams.set("name", title.value.name);
-ogImage.searchParams.set("format", title.value.expand!.format.name);
-if (title.value.cover)
+ogImage.searchParams.set("name", data.value.name);
+ogImage.searchParams.set("format", data.value.expand!.format.name);
+if (data.value.cover)
   ogImage.searchParams.set(
     "image",
-    $pb.files.getUrl(title.value, title.value.cover),
+    $pb.files.getUrl(data.value, data.value.cover),
   );
 
 useSeoMeta({
-  title: title.value.name,
-  description: title.value.description.replace(/<[^>]*>/g, "").slice(0, 200),
-  ogTitle: title.value.name,
-  ogDescription: title.value.description.replace(/<[^>]*>/g, "").slice(0, 200),
+  title: data.value.name,
+  description: data.value.description.replace(/<[^>]*>/g, "").slice(0, 200),
+  ogTitle: data.value.name,
+  ogDescription: data.value.description.replace(/<[^>]*>/g, "").slice(0, 200),
   ogImage: ogImage.toString(),
-  ogImageAlt: title.value.name,
+  ogImageAlt: data.value.name,
 });
 
 definePageMeta({
@@ -113,8 +84,8 @@ definePageMeta({
 </script>
 
 <template>
-  <UContainer v-if="title">
-    <PageTitleHeader :title="title" />
+  <UContainer v-if="data">
+    <PageTitleHeader :title="data" />
 
     <div class="mt-6 flex flex-col-reverse gap-6 lg:flex-row">
       <div class="flex-1">
@@ -139,33 +110,19 @@ definePageMeta({
           <UButton class="flex-1" block disabled>
             {{ $t("general.follow") }}
           </UButton>
-          <UButton
-            v-if="shareSupported"
-            icon="i-fluent-share-ios-20-regular"
-            color="gray"
-            square
-            @click="startShare"
-          />
-          <UButton
-            v-else-if="clipboardSupported"
-            :icon="
-              copied
-                ? 'i-fluent-checkmark-20-filled'
-                : 'i-fluent-link-square-20-filled'
-            "
-            color="gray"
-            square
-            @click="copyLink"
-          />
+          <AppShareButton :title="data.name" />
         </div>
+
         <PageTitleWorks v-if="works && works.length > 0" :works="works" />
+
         <UButton
-          :to="`/review/create?title=${title.id}`"
+          :to="`/review/create?title=${data.id}`"
           color="gray"
           icon="i-fluent-pen-20-filled"
           block
-          >{{ $t("review.create") }}</UButton
         >
+          {{ $t("review.create") }}
+        </UButton>
       </div>
     </div>
   </UContainer>
