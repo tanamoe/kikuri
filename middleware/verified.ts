23ade9441d17c8a826bbc9d70d3ba9b3b4028bb9
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-10-25 15:48:46 +0700

    fix: verified middleware

--- middleware/verified.ts
+++ middleware/verified.ts
@@ -3,6 +3,10 @@ import type { UsersResponse } from "~/types/pb";
 export default defineNuxtRouteMiddleware(() => {
   const { $pb } = useNuxtApp();
 
+  if (!$pb.authStore.isAuthRecord) {
+    return navigateTo("/login");
+  }
+
   if (!($pb.authStore.model as UsersResponse).verified) {
     return navigateTo("/verification");
   }
