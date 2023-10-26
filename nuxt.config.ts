463d978f6fff99bc08323ecd05cdf3104d45d56e
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-10-26 22:52:42 +0700

    feat: add cache header for title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,6 +32,7 @@ export default defineNuxtConfig({
   routeRules: {
     "/": { prerender: true, isr: 3600 },
     "/calendar": { prerender: true, isr: 3600 },
+    "/title/*": { isr: 3600 },
   },
   image: {
     provider: "imagor",
