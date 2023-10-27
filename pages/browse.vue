3ccfb1b42b3123d5f64601ec477706ed7d42db83
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-10-27 17:10:36 +0700

    feat(browse): progress

--- pages/browse.vue
+++ pages/browse.vue
@@ -1,22 +1,28 @@
 <script setup lang="ts">
-import { Collections, type TitlesResponse } from "~/types/pb";
+import { Collections, type TitlesResponse } from "@/types/pb";
+import type { MetadataCommon } from "@/types/common";
 
 const { $pb } = useNuxtApp();
+const store = useAdvancedFilterStore();
 
+const { formats, publishers } = storeToRefs(store);
 const query = ref("");
 const queryDebounced = refDebounced(query, 500);
 const page = ref(1);
 
 const filter = computed(() => ({
   name: queryDebounced.value,
+  publishers: publishers.value.map((publishers) => publishers.id),
 }));
 
 const { data: titles, pending } = await useAsyncData(
   () =>
-    $pb.collection(Collections.Titles).getList<TitlesResponse>(page.value, 24, {
-      filter: $pb.filter("name ~ {:name}", filter.value),
-      expand: "title",
-    }),
+    $pb
+      .collection(Collections.Titles)
+      .getList<TitlesResponse<MetadataCommon>>(page.value, 24, {
+        filter: $pb.filter("name ~ {:name}", filter.value),
+        expand: "title",
+      }),
   {
     watch: [filter],
   },
@@ -26,18 +32,22 @@ const { data: titles, pending } = await useAsyncData(
 <template>
   <UContainer>
     <AppH1 class="mb-6">{{ $t("general.browse") }}</AppH1>
-    <UInput
-      v-model="query"
-      size="lg"
-      icon="i-fluent-search-24-filled"
-      :placeholder="$t('general.searchPlaceholder')"
-    />
-    <div class="mt-6 flex justify-between">
-      <AppFilterAdvanced />
-      <USelect
-        :placeholder="$t('general.sortBy')"
-        icon="i-fluent-arrow-sort-down-lines-20-filled"
+
+    <div class="space-y-6">
+      <UInput
+        v-model="query"
+        size="lg"
+        icon="i-fluent-search-24-filled"
+        :placeholder="$t('general.searchPlaceholder')"
       />
+      <div class="flex justify-between gap-3">
+        <AppFilterAdvanced />
+        <USelect
+          class="flex-shrink-0"
+          :placeholder="$t('general.sortBy')"
+          icon="i-fluent-arrow-sort-down-lines-20-filled"
+        />
+      </div>
     </div>
 
     <div v-if="pending" class="mt-6 grid grid-cols-6 gap-x-6 gap-y-12">
