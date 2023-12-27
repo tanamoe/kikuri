510f44dbe94d27080afd4784b2255842f6627c7c
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-12-27 23:55:14 +0700

    feat(login): update social login

---
+++ components/app/sign-in/AppSignInButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const ui = {
+  block: "gap-2.5",
+  padding: {
+    md: "px-3 py-2.5",
+  },
+  color: {
+    gray: {
+      solid:
+        "ring-0 bg-gray-200 dark:bg-gray-700 hover:bg-gray-200/75 dark:hover:bg-gray-700/50",
+    },
+  },
+};
+</script>
+
+<template>
+  <UButton color="gray" block :ui="ui" size="md">
+    <div class="flex-1 text-center">
+      <slot />
+    </div>
+  </UButton>
+</template>
---
+++ components/app/sign-in/AppSignInDiscordButton.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const ui = {
+  block: "gap-2.5",
+  padding: {
+    md: "px-3 py-2.5",
+  },
+  color: {
+    gray: {
+      solid:
+        "ring-0 bg-gray-200 dark:bg-gray-700 hover:bg-gray-200/75 dark:hover:bg-gray-700/50",
+    },
+  },
+  icon: {
+    base: "text-[#5865F2]",
+  },
+};
+</script>
+
+<template>
+  <UButton color="gray" block :ui="ui" icon="i-simple-icons-discord" size="md">
+    <div class="flex-1 text-center">
+      {{
+        $t("auth.loginWith", {
+          name: "Discord",
+        })
+      }}
+    </div>
+  </UButton>
+</template>
