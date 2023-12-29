92e4abc082085c0907c674b19d0a7be0d9c78564
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-12-29 13:48:40 +0700

    fix(library): redesign

--- components/page/library/PageLibraryBooks.vue
+++ components/page/library/PageLibraryBooks.vue
@@ -3,8 +3,6 @@ import type { LibraryBookEdit, LibraryBookRemove } from "#build/components";
 import type { UserCollectionBooksResponse } from "@/types/collections";
 
 const { $pb } = useNuxtApp();
-const { t } = useI18n({ useScope: "global" });
-const settingsStore = useSettingsStore();
 
 const props = defineProps<{
   editModal?: InstanceType<typeof LibraryBookEdit>;
@@ -13,9 +11,12 @@ const props = defineProps<{
   editable: boolean;
 }>();
 
-const rows = computed(() =>
-  props.books.items.map((item) => ({
-    id: item.bookId,
+const query = ref("");
+const queryDebounced = refDebounced(query, 500);
+
+const rows = computed(() => {
+  return props.books.items.map((item) => ({
+    id: item.id,
     cover: item.book?.covers
       ? $pb.files.getUrl(
           {
@@ -28,83 +29,13 @@ const rows = computed(() =>
     name: item.book!.publication.name,
     edition: item.book!.edition,
     digital: item.book!.publication.digital,
-    publishDate: {
-      value: item.book!.publishDate,
-      class: "whitespace-nowrap",
-    },
+    publishDate: item.book!.publishDate,
     quantity: item.quantity,
-    price: {
-      value: item.book!.price,
-      class: "whitespace-nowrap",
-    },
+    price: item.book!.price,
     status: item.status,
     collection: item.collectionId,
-    created: {
-      value: item.created,
-      class: "whitespace-nowrap",
-    },
-    updated: {
-      value: item.updated,
-      class: "whitespace-nowrap",
-    },
-  })),
-);
-
-const columns = computed(() =>
-  [
-    {
-      key: "cover",
-      label: t("general.coverImages"),
-      class: "whitespace-nowrap",
-    },
-    {
-      key: "name",
-      label: t("general.title"),
-      sortable: true,
-    },
-    {
-      key: "quantity",
-      label: t("general.quantity"),
-      class: "whitespace-nowrap",
-      sortable: true,
-    },
-    {
-      key: "price",
-      label: t("general.price"),
-      class: "whitespace-nowrap",
-      sortable: true,
-    },
-    {
-      key: "publishDate",
-      label: t("general.publishDate"),
-      class: "whitespace-nowrap",
-      sortable: true,
-    },
-    {
-      key: "status",
-      label: t("general.status"),
-      class: "whitespace-nowrap",
-      sortable: true,
-    },
-    {
-      key: "created",
-      label: t("library.created"),
-      class: "whitespace-nowrap",
-      sortable: true,
-    },
-    {
-      key: "updated",
-      label: t("library.updated"),
-      class: "whitespace-nowrap",
-      sortable: true,
-    },
-    {
-      key: "actions",
-      label: t("library.actions"),
-      class: "whitespace-nowrap text-right",
-    },
-  ].filter((col) => settingsStore.library.columns.includes(col.key)),
-);
+  }));
+});
 
 function handleEdit(row: (typeof rows.value)[0]) {
   if (props.editModal) {
@@ -138,87 +69,32 @@ function handleRemove(row: (typeof rows.value)[0]) {
 </script>
 
 <template>
-  <div
-    class="overflow-hidden rounded-md border border-gray-200 dark:border-gray-800"
-  >
-    <UTable
-      :rows="rows || []"
-      :columns="columns"
-      class="[font-feature-settings:'ss01']"
-      :ui="{
-        thead: 'bg-gray-200 dark:bg-gray-800',
-        tr: {
-          base: 'hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors',
-        },
-        wrapper: 'relative overflow-x-auto',
-        td: { base: 'whitespace-nowrap lg:whitespace-normal' },
-      }"
+  <div class="grid grid-cols-1 gap-6">
+    <UInput v-model="query" :placeholder="$t('general.search')" />
+
+    {{ queryDebounced }}
+
+    <UCard
+      v-for="item in rows"
+      :key="item.id"
+      :ui="{ body: { base: 'flex gap-3 text-sm' } }"
     >
-      <template #cover-data="{ row }">
+      <div class="w-20 md:w-16 lg:w-14 xl:w-12">
         <img
-          v-if="row.cover"
-          class="aspect-[2/3] h-full max-h-16 rounded-md object-cover"
-          :src="row.cover"
+          v-if="item.cover"
+          class="rounded-md"
+          :src="item.cover"
+          :name="item.name"
         />
-      </template>
-
-      <template #name-data="{ row }">
-        <div
-          v-if="row.edition || row.digital"
-          class="mb-2 flex items-center gap-3 whitespace-nowrap"
-        >
-          <UBadge v-if="row.edition" color="tanaamber">
-            {{ row.edition }}
-          </UBadge>
-          <UBadge v-if="row.digital" color="red">Digital</UBadge>
-        </div>
-        <div class="flex items-center gap-3">
-          <div>{{ row.name }}</div>
-        </div>
-      </template>
-
-      <template #quantity-data="{ row }">
-        <span>{{ row.quantity }}</span>
-      </template>
-
-      <template #price-data="{ row }">
-        <span>{{ $n(row.price.value, "currency", "vi") }}</span>
-      </template>
-
-      <template #publishDate-data="{ row }">
-        <span v-if="row.publishDate.value">
-          {{ $d(new Date(row.publishDate.value), "publishDate") }}
-        </span>
-        <span v-else></span>
-      </template>
-
-      <template #created-data="{ row }">
-        <span v-if="row.created.value">
-          {{ $d(new Date(row.created.value), "publishDate") }}
-        </span>
-        <span v-else></span>
-      </template>
-
-      <template #updated-data="{ row }">
-        <span v-if="row.updated.value">
-          {{ $d(new Date(row.updated.value), "publishDate") }}
-        </span>
-        <span v-else></span>
-      </template>
-
-      <template #status-data="{ row }">
-        <span>{{ $t(`status.${row.status.toLowerCase()}`) }}</span>
-      </template>
-
-      <template #actions-data="{ row }: { row: (typeof rows.value)[0] }">
-        <div v-if="editable" class="whitespace-nowrap text-right">
+      </div>
+      <div class="w-full">
+        <div v-if="editable" class="float-right">
           <UButton
             v-if="editModal"
             icon="i-fluent-edit-20-filled"
             color="gray"
             variant="ghost"
             square
-            @click="handleEdit(row)"
           />
           <UButton
             v-if="removeModal"
@@ -226,11 +102,37 @@ function handleRemove(row: (typeof rows.value)[0]) {
             color="red"
             variant="ghost"
             square
-            @click="handleRemove(row)"
           />
         </div>
-        <div v-else></div>
-      </template>
-    </UTable>
+
+        <div class="space-y-2">
+          <div v-if="item.edition || item.digital">
+            <UBadge v-if="item.edition" color="tanaamber">
+              {{ item.edition }}
+            </UBadge>
+            <UBadge v-if="item.digital" color="red"> Digital </UBadge>
+          </div>
+          <h3 class="font-bold">
+            {{ item.name }}
+          </h3>
+          <div v-if="item.publishDate" class="text-gray-600 dark:text-gray-400">
+            Phát hành {{ $d(new Date(item.publishDate), "publishDate") }}
+          </div>
+          <div class="flex gap-2 text-gray-600 dark:text-gray-400">
+            <div>
+              {{ $t(`status.${item.status.toLowerCase()}`) }}
+            </div>
+            &middot;
+            <div>
+              <span v-if="item.price">
+                {{ $n(item.price, "currency", "vi") }}
+              </span>
+              &times;
+              {{ item.quantity }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </UCard>
   </div>
 </template>
