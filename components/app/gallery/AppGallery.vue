f7616bf0a57b5f5ac5b3762796087281653d6c4d
Nguyen Do <khoanguyen.do@outlook.com>
2024-02-10 02:26:22 +0700

    refactor: migrate component to `@nuxt/ui@2.13`

--- components/app/gallery/AppGallery.vue
+++ components/app/gallery/AppGallery.vue
@@ -12,16 +12,19 @@ const { open } = store;
 const { images } = storeToRefs(store);
 
 images.value = props.items;
+
+const ui = {
+  base: "relative overflow-hidden cursor-zoom-in",
+  shadow: "shadow",
+  body: {
+    padding: "p-0 sm:p-0",
+  },
+};
 </script>
 
 <template>
   <div class="grid grid-cols-2 gap-6 sm:grid-cols-4 xl:grid-cols-6">
-    <AppCard
-      v-for="(image, i) in images"
-      :key="i"
-      class="cursor-zoom-in"
-      @click="open(i)"
-    >
+    <UCard v-for="(image, i) in images" :key="i" :ui="ui" @click="open(i)">
       <img
         loading="lazy"
         class="aspect-[2/3] h-full w-full object-cover transition-all hover:brightness-90 dark:hover:brightness-75"
@@ -30,7 +33,7 @@ images.value = props.items;
         sizes="(max-width: 640px) 40vw, (max-width: 768px) 20vw, 15vw"
         v-bind="$attrs"
       />
-    </AppCard>
+    </UCard>
 
     <AppGalleryLightbox />
   </div>
