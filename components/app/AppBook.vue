cec885fa610d991086eb6f20f740faeaecaa1664
Nguyen Do <khoanguyen.do@outlook.com>
2024-07-24 01:33:28 +0700

    fix(book): update according types change

--- components/app/AppBook.vue
+++ components/app/AppBook.vue
@@ -1,8 +1,28 @@
 <script setup lang="ts">
 import { joinURL } from "ufo";
-import type { BookDetailsCommon } from "@/types/common";
+import type { BooksCommon, MetadataImages } from "@/types/common";
 
 const store = useSettingsStore();
+
+export interface Props {
+  book: BooksCommon;
+  showButton?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  showButton: true,
+});
+
+defineEmits<{
+  add: [BooksCommon];
+}>();
+
+const publication = props.book.expand?.publication;
+const release = publication?.expand?.release;
+const title = release?.expand?.title;
+const image = props.book.expand?.assets_via_book?.[0].resizedImage as
+  | MetadataImages
+  | undefined;
 
 const ui = {
   base: "relative overflow-hidden",
@@ -11,30 +31,13 @@ const ui = {
     padding: "p-0 sm:p-0",
   },
 };
-
-export interface Props {
-  book: BookDetailsCommon;
-  showButton?: boolean;
-}
-
-withDefaults(defineProps<Props>(), {
-  showButton: true,
-});
-
-defineEmits<{
-  add: [BookDetailsCommon];
-}>();
 </script>
 
 <template>
-  <NuxtLink
-    v-if="book.expand?.publication && book.expand.release?.expand?.title"
-    :to="joinURL('/title/' + book.expand.release.expand.title.slug)"
-    class="group"
-  >
+  <NuxtLink :to="title && joinURL('/title/' + title.slug)" class="group">
     <UCard :ui="ui">
       <UBadge
-        v-if="book.expand.publication.digital == true"
+        v-if="release && release.digital == true"
         class="absolute right-2 top-2 z-20 text-gray-900"
         color="red"
       >
@@ -104,17 +107,9 @@ defineEmits<{
 
       <AppCover
         class="relative z-10 transition-all group-hover:brightness-90 dark:group-hover:brightness-75"
-        :name="book.expand.publication.name"
-        :src="
-          book.metadata?.images && Array.isArray(book.metadata.images)
-            ? book.metadata.images[0]['1920w']
-            : undefined
-        "
-        :srcset="
-          book.metadata?.images && Array.isArray(book.metadata.images)
-            ? book.metadata.images[0]
-            : undefined
-        "
+        :name="publication?.name"
+        :src="image && image['1920w']"
+        :srcset="image"
         v-bind="$attrs"
       />
 
@@ -124,30 +119,31 @@ defineEmits<{
     <div v-if="store.display.bookDetails" class="mt-2">
       <div
         v-if="
-          book.expand.publication.volume < 90000000 &&
-          book.expand.publication.volume > 0
+          publication &&
+          publication?.volume < 90000000 &&
+          publication.volume > 0
         "
         class="space-y-1"
       >
         <div
-          v-if="book.expand?.release.expand?.title.name"
+          v-if="title?.name"
           class="decoration-primary-400 line-clamp-4 font-condensed text-xl font-black decoration-[.2rem] underline-offset-[.2rem] group-hover:underline"
         >
-          {{ book.expand.release.expand.title.name }}
+          {{ title.name }}
         </div>
         <div class="text-gray-500 dark:text-gray-400">
           {{
             $t("general.volumeNumber", {
-              volume: parseVolume(book.expand.publication.volume),
+              volume: parseVolume(publication.volume),
             })
           }}
         </div>
       </div>
-      <div v-else>
+      <div v-else-if="publication">
         <span
           class="decoration-primary-400 font-condensed text-xl font-black decoration-[.2rem] underline-offset-[.2rem] group-hover:underline"
         >
-          {{ book.expand.publication.name }}
+          {{ publication.name }}
         </span>
       </div>
     </div>
