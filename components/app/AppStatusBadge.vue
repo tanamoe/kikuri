06fceab5a026dae41c786d9a3ebf646efaff46d0
Nguyen Do <khoanguyen.do@outlook.com>
2024-08-02 03:46:10 +0700

    feat: release status badge component

---
+++ components/app/AppStatusBadge.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { ReleasesStatusOptions } from "~/types/pb";
+
+const { releaseStatus } = useOptions();
+
+const props = defineProps<{
+  status: ReleasesStatusOptions;
+}>();
+
+const rs = computed(() =>
+  releaseStatus.value.find((s) => s.id === props.status),
+);
+</script>
+
+<template>
+  <UBadge v-if="rs" :color="rs.color">
+    {{ rs.label }}
+  </UBadge>
+</template>
