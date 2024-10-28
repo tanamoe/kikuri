e974e118d2153fff19d6b98cfb6c0b65ae8b3e3f
Nguyen Do <khoanguyen.do@outlook.com>
2024-10-29 01:16:59 +0700

    feat(components): implement title link component

---
+++ components/app/title/AppTitleLinkButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { LinkSourcesResponse, LinksResponse } from "@/types/pb";
+
+defineProps<{
+  link: LinksResponse<{ source: LinkSourcesResponse }>;
+}>();
+</script>
+
+<template>
+  <UButton
+    v-if="link.expand?.source"
+    :to="link.url"
+    target="_blank"
+    color="black"
+  >
+    <img
+      v-if="link.expand.source.icon"
+      :src="
+        $pb.files.getUrl(link.expand.source, link.expand.source.icon, {
+          thumb: '50x50',
+        })
+      "
+      class="h-4 w-4 object-contain"
+      :alt="link.expand.source.name"
+    />
+    {{ link.expand.source.name }}
+  </UButton>
+</template>
