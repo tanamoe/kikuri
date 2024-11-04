95e438549d653d1c72f1d4b7ef48ff2c76759bfc
Nguyen Do <khoanguyen.do@outlook.com>
2024-11-05 00:32:35 +0700

    feat(library): implement group and perpage option components

---
+++ components/input/library/InputLibraryGroupOptions.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const { t } = useI18n();
+
+const model = defineModel<string>();
+
+const options = computed(() => [
+  {
+    id: "none",
+    label: t("settings.groupBy.none"),
+  },
+  {
+    id: "release",
+    label: t("settings.groupBy.release"),
+  },
+  {
+    id: "status",
+    label: t("settings.groupBy.status"),
+  },
+]);
+
+const uiMenu = {
+  width: "w-48",
+};
+</script>
+
+<template>
+  <USelectMenu
+    v-model="model"
+    :options
+    value-attribute="id"
+    option-attribute="label"
+    :ui-menu
+  >
+    <UButton color="gray" icon="i-fluent-group-list-20-filled">
+      {{ $t("general.group") }}
+    </UButton>
+  </USelectMenu>
+</template>
