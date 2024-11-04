6057ed2b6af273a4caca41050c9b009ad6b8c99b
Nguyen Do <khoanguyen.do@outlook.com>
2024-11-05 00:31:25 +0700

    feat(components): library edit & delete button

---
+++ components/library/LibraryRemoveButton.client.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ModalCollectionBookRemove } from "#components";
+
+const modal = useModal();
+
+interface Props {
+  id: string;
+  name: string;
+  collection: string;
+  square?: boolean;
+}
+
+const { id, name, collection } = defineProps<Props>();
+
+const emit = defineEmits<{
+  change: [];
+}>();
+
+function remove() {
+  modal.open(ModalCollectionBookRemove, {
+    book: {
+      id,
+      name,
+    },
+    collection,
+    callback: () => emit("change"),
+  });
+}
+</script>
+
+<template>
+  <UButton
+    icon="i-fluent-delete-20-filled"
+    v-bind="$attrs"
+    square
+    @click.prevent="remove"
+  />
+</template>
