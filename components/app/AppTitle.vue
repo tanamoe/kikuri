0a0cd8cba665ef0499e21f9e53ab01451b0ce669
Nguyen Do <khoanguyen.do@outlook.com>
2024-11-04 21:18:37 +0700

    feat(components): implements title and release

--- components/app/AppTitle.vue
+++ components/app/AppTitle.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import { joinURL } from "ufo";
-import type { MetadataCommon, MetadataImages } from "@/types/common";
-import type { TitlesResponse } from "@/types/pb";
+import type { MetadataImages } from "@/types/common";
+import type {
+  AssetsResponse,
+  FormatsResponse,
+  TitlesResponse,
+} from "@/types/pb";
 
 const ui = {
   base: "relative overflow-hidden",
@@ -12,42 +16,42 @@ const ui = {
 };
 
 defineProps<{
-  title: TitlesResponse<MetadataCommon>;
-  src?: string;
-  srcset?: MetadataImages;
+  title: Pick<TitlesResponse, "name" | "slug">;
+  format?: Pick<FormatsResponse, "name">;
+  image?: AssetsResponse<MetadataImages>;
+  sizes?: string;
+  draggable?: boolean;
 }>();
 </script>
 
 <template>
-  <NuxtLink :to="joinURL('/title/' + title.slug)" class="group space-y-3">
-    <UCard :ui="ui">
-      <AppBookCover
+  <NuxtLink
+    :to="joinURL('/title', title.slug)"
+    class="group space-y-3"
+    :draggable
+  >
+    <UCard :ui>
+      <AppImageCover
         class="transition-all group-hover:brightness-90 dark:group-hover:brightness-75"
         :name="title.name"
-        :src="
-          src ||
-          (title.metadata?.images && !Array.isArray(title.metadata.images)
-            ? title.metadata.images['1920w']
-            : undefined)
-        "
-        :srcset="
-          srcset ||
-          (title.metadata?.images && !Array.isArray(title.metadata.images)
-            ? title.metadata.images
-            : undefined)
-        "
-        v-bind="$attrs"
+        :src="image && $pb.files.getUrl(image, image.image)"
+        :srcset="image && image.resizedImage"
+        :sizes
+        :draggable
       />
     </UCard>
     <div class="space-y-1">
-      <slot name="before" />
+      <div class="flex flex-wrap items-center gap-1">
+        <UBadge v-if="format" color="gray">
+          {{ format.name }}
+        </UBadge>
+      </div>
+
       <h3
         class="decoration-primary-400 line-clamp-4 font-condensed text-xl font-black decoration-[.2rem] underline-offset-[.2rem] group-hover:underline"
       >
         {{ title.name }}
       </h3>
-
-      <slot name="after" />
     </div>
   </NuxtLink>
 </template>
