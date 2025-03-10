5d9cd8e7212c7bd1886b1d6b3528418c726fa33f
Nguyen Do <khoanguyen.do@outlook.com>
2025-03-10 13:53:40 +0700

    build: ignore _ as unused

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,15 @@
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt();
+export default withNuxt({
+  rules: {
+    "vue/html-self-closing": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+  },
+});
